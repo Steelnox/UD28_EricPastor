23e0a37219eb6b1e1478ae2656725c84af472e3f
Steelnox <eric14pastor@gmail.com>
2021-02-14 20:06:57 +0100

    Ejercicio 27_2 modificado para la unidad 28

---
+++ UD27_Ej2/UD27_Ej2/Models/Cientifico.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UD27_Ej2.Models
+{
+    public class Cientifico
+    {
+        public Cientifico()
+        {
+            Asignados_a = new HashSet<Asignado_a>();
+        }
+        public string DNI { get; set; }
+        public string NomApels { get; set; }
+        public ICollection<Asignado_a> Asignados_a { get; set; }
+    }
+}
