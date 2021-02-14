b9396b6d77b983f88348b790fdbc852cfae9fae0
Steelnox <eric14pastor@gmail.com>
2021-02-14 19:49:58 +0100

    Ejercicio UD27_1 adaptado para la unidad 28

---
+++ UD27_Ej1/UD27_Ej1/Models/APIContext.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace UD27_Ej1.Models
+{
+    public class APIContext : DbContext
+    {
+        public APIContext(DbContextOptions<APIContext> options)
+            : base(options)
+        {
+
+        }
+
+        public virtual DbSet<Pieza> Piezas { get; set; }
+        public virtual DbSet<Proveedor> Proveedores { get; set; }
+        public virtual DbSet<Suministra> Suministras { get; set; }
+        public virtual DbSet<UserInfo> UserInfo { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Pieza>(entity =>
+            {
+
+                entity.ToTable("Piezas");
+                entity.HasKey(e => e.Codigo);
+                entity.Property(e => e.Codigo).HasColumnName("Codigo");
+
+                entity.Property(e => e.Nombre).HasColumnName("Nombre")
+                .IsRequired()
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Proveedor>(entity =>
+            {
+                entity.ToTable("Proveedores");
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Id).HasColumnName("Id")
+                .HasMaxLength(4);
+
+                entity.Property(e => e.Nombre).HasColumnName("Nombre")
+                .IsRequired()
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            }
+            );
+
+            modelBuilder.Entity<Suministra>(entity =>
+            {
+                entity.ToTable("Suministra");
+                entity.HasKey(e => e.CodigoPieza);
+                entity.HasKey(j => j.IdProveedor);
+
+                entity.Property(e => e.CodigoPieza).HasColumnName("CodigoPieza");
+
+                entity.Property(e => e.IdProveedor).HasColumnName("IdProveedor");
+
+                entity.HasOne(d => d.Pieza)
+                .WithMany(e => e.Suministras)
+                .HasForeignKey(d => d.CodigoPieza)
+                .OnDelete(DeleteBehavior.ClientSetNull);
+
+                entity.HasOne(d => d.Proveedor)
+                .WithMany(e => e.Suministras)
+                .HasForeignKey(d => d.IdProveedor)
+                .OnDelete(DeleteBehavior.ClientSetNull);
+            }
+            );
+
+            modelBuilder.Entity<UserInfo>(entity =>
+            {
+                entity.HasKey(e => e.UserId)
+                    .HasName("PK__UserInfo__1788CC4CE81A3218");
+
+                entity.Property(e => e.CreatedDate)
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.FirstName)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.LastName)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Password)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.UserName)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+            });
+
+        }
+
+    }
+}
---
+++ UD27_Ej1/UD27_Ej1/Models/Pieza.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UD27_Ej1.Models
+{
+    public class Pieza
+    {
+        public Pieza()
+        {
+            Suministras = new HashSet<Suministra>();
+        }
+        public int Codigo { get; set; }
+        public string Nombre { get; set; }
+        public ICollection<Suministra> Suministras { get; set; }
+    }
+}
